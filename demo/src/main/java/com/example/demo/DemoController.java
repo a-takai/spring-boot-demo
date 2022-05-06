c590e416558a60add528f51f9272da43f0d5c08b
akitotakai <takai1414@icloud.com>
2022-05-06 18:43:39 +0900

    Angularとの連携部

---
+++ demo/src/main/java/com/example/demo/DemoController.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+    @Autowired
+    DemoService demoService;
+
+    @RequestMapping("api/data")
+    public String getString() {
+        return demoService.getString();
+    }
+}
